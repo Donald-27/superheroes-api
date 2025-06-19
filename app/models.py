9b71fa0948963cb0440743f9b50c73e230d41f71
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-19 21:49:16 +0300

    created the power model with the basic fields and relationships

--- app/models.py
+++ app/models.py
@@ -10,32 +10,3 @@ class Hero(db.Model):
     hero_powers = db.relationship('HeroPower', back_populates='hero', cascade='all, delete-orphan')
 
 
-class Power(db.Model):
-    __tablename__ = 'powers'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-
-    hero_powers = db.relationship('HeroPower', back_populates='power', cascade='all, delete-orphan')
-
-    __table_args__ = (
-        db.CheckConstraint('length(description) >= 20'),
-    )
-
-
-class HeroPower(db.Model):
-    __tablename__ = 'hero_powers'
-
-    id = db.Column(db.Integer, primary_key=True)
-    strength = db.Column(db.String, nullable=False)
-
-    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
-    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
-
-    hero = db.relationship('Hero', back_populates='hero_powers')
-    power = db.relationship('Power', back_populates='hero_powers')
-
-    __table_args__ = (
-        db.CheckConstraint("strength IN ('Strong', 'Weak', 'Average')"),
-    )
