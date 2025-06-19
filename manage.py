683e9631b8234a493b796bd1e0589937f65ea50c
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-19 23:41:25 +0300

    corrected a missing underscore

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@ from app import create_app
 from app.extensions import db
 from flask_migrate import Migrate
 
-# Step 1: Create the Flask app using the factory function
 app = create_app()
 migrate = Migrate(app, db)
 
