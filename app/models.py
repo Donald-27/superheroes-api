f3aa4fba540eed459655b5d0df28726ddd5668d7
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 07:28:20 +0300

    fixed a typoin models.py

--- app/models.py
+++ app/models.py
@@ -1,23 +1,41 @@
-from .extensions import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class Hero(db.Model):
+    __tablename__ = 'heroes'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     super_name = db.Column(db.String, nullable=False)
 
-    hero_powers = db.relationship('HeroPower', backref='hero')
-
+    hero_powers = db.relationship('HeroPower', back_populates='hero', cascade='all, delete-orphan')
 
 class Power(db.Model):
+    __tablename__ = 'powers'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
-    hero_powers = db.relationship('HeroPower', backref='power')
 
+    hero_powers = db.relationship('HeroPower', back_populates='power', cascade='all, delete-orphan')
+
+    __table_args__ = (
+        db.CheckConstraint('LENGTH(description) >= 20', name='check_description_length'),
+    )
 
 class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String, nullable=False)
 
-    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
-    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
+
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='hero_powers')
+
+    __table_args__ = (
+        db.CheckConstraint("strength IN ('Strong', 'Weak', 'Average')", name='check_strength_values'),
+    )
