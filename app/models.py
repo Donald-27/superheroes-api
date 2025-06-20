d6248d21600fcd5ae1808d40d566f1f7c01e674b
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 07:25:37 +0300

    modified models.py added the table args

--- app/models.py
+++ app/models.py
@@ -1,37 +1,23 @@
 from .extensions import db
 
 class Hero(db.Model):
-    __tablename__ = 'heroes'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     super_name = db.Column(db.String, nullable=False)
-    hero_powers = db.relationship('HeroPower', back_populates='hero', cascade='all, delete-orphan')
+
+    hero_powers = db.relationship('HeroPower', backref='hero')
 
 
 class Power(db.Model):
-    __tablename__ = 'powers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
+    hero_powers = db.relationship('HeroPower', backref='power')
 
-    hero_powers = db.relationship('HeroPower', back_populates='power', cascade='all, delete-orphan')
 
-    __table_args__ = (
-        db.CheckConstraint('length(description) >= 20'),
-    )
 class HeroPower(db.Model):
-    __tablename__ = 'hero_powers'
-
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String, nullable=False)
 
-    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
-    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
-
-    hero = db.relationship('Hero', back_populates='hero_powers')
-    power = db.relationship('Power', back_populates='hero_powers')
-
-    __table_args__ = (
-        db.CheckConstraint("strength IN ('Strong', 'Weak', 'Average')"),
-    )
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
